752690005c4c648405ac3337e2378f16658a27ef
Reinny <reynsth@gmail.com>
2021-03-08 15:01:08 -0400

    :lipstick: Styling total expenses count

--- src/components/ExpensesTotal.js
+++ src/components/ExpensesTotal.js
@@ -4,6 +4,7 @@ import store from "../store/index";
 export default class ExpensesTotal extends Component {
 	constructor() {
 		super({ store, element: document.createElement("h2") });
+		this.element.classList.add("expenses-total");
 	}
 
 	render() {
