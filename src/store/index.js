1ce26fb5ae912de6c9b28a20f7223be74def425e
Reinny <reynsth@gmail.com>
2021-03-07 22:09:52 -0400

    :fire: Separating store state

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import Store from "./store";
+import state from "./state";
 import mutations from "./mutations";
 
 export default new Store({
-	state: { expenses: [] },
+	state,
 	mutations,
 });
---
+++ src/store/state.js
@@ -0,0 +1,5 @@
+const state = {
+	expenses: [],
+};
+
+export default state;
