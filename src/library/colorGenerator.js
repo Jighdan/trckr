262c27db9ad3ff74590f20057cd9ce1a35a1c170
Reinny <reynsth@gmail.com>
2021-03-08 12:30:36 -0400

    :sparkles: Adding a color generator

---
+++ src/library/colorGenerator.js
@@ -0,0 +1,16 @@
+const colors = [
+	"#F94144",
+	"#F3722C",
+	"#F8961E",
+	"#F9C74F",
+	"#90BE6D",
+	"#43AA8B",
+	"#577590",
+];
+
+const getRandomColor = () => {
+	const randomIndex = Math.floor(Math.random() * colors.length);
+	return colors[randomIndex];
+};
+
+export default getRandomColor;
