8e6317fabf6266a9a9ec97798ccb027699f432ac
Reinny <reynsth@gmail.com>
2021-03-08 11:12:03 -0400

    Missed file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import ExpenseForm from "./components/ExpenseForm";
 import ExpensesList from "./components/ExpensesList";
+import ExpensesTotal from "./components/ExpensesTotal";
 
 const root = document.getElementById("root");
 
 const instanceOfExpenseForm = new ExpenseForm();
+const instanceOfExpensesTotal = new ExpensesTotal();
 const instanceOfExpensesList = new ExpensesList();
 
 window.addEventListener("DOMContentLoaded", () => {
-	root.append(instanceOfExpenseForm.render(), instanceOfExpensesList.render());
+	root.append(
+		instanceOfExpenseForm.render(),
+		instanceOfExpensesTotal.render(),
+		instanceOfExpensesList.render()
+	);
 });
