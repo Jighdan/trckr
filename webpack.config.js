72cb79b3b1b63183de3cb4ff070b1d2f65f9d0d1
Reinny <reynsth@gmail.com>
2021-03-09 09:36:46 -0400

    :wrench: Setting up configuration files

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
 const path = require("path");
 
 module.exports = {
-	entry: "./src/index.js",
-	mode: process.env.NODE_ENV === "production" ? "production" : "development",
-	module: {
-		rules: [],
-	},
+	entry: "./src/index.ts",
 	output: {
-		path: path.resolve(__dirname, "build"),
 		filename: "script.js",
+		path: path.resolve(__dirname, "build"),
+	},
+	devtool: "source-map",
+	mode: process.env.NODE_ENV === "production" ? "production" : "development",
+
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: "ts-loader",
+			},
+			{
+				test: /\.js$/,
+				loader: "source-map-loader",
+			},
+		],
+	},
+	resolve: {
+		extensions: [".ts", ".js"],
 	},
 };
