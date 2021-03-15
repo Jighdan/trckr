d32219eca2a33fa72020736ee09006c9537456b1
Reinny <reynsth@gmail.com>
2021-03-15 11:30:37 -0400

    :fire: What the heck was that code doeing there?

--- src/components/Category/CategoryForm/CategoryFormParent.ts
+++ src/components/Category/CategoryForm/CategoryFormParent.ts
@@ -1,51 +1,5 @@
 import { InterfaceCategory } from "../../../models/Category";
 
-import { InterfaceType } from "../../../models/Type";
-
-const composeCategoryFormTypeIcon = (iconPath: string): HTMLDivElement => {
-	const element = document.createElement("div");
-	element.classList.add("category-form-type-icon");
-	const maskUrl = `url(${iconPath}) no-repeat center`;
-	element.style.mask = maskUrl;
-	element.style.webkitMask = maskUrl;
-
-	return element;
-};
-
-const composeCategoryFormTypeOption = (typeId: string, type: InterfaceType): HTMLDivElement => {
-	const elementContainer = document.createElement("div");
-	const inputElement = document.createElement("input");
-	const labelElement = document.createElement("label");
-	const iconElement = composeCategoryFormTypeIcon(type.icon);
-
-	inputElement.setAttribute("type", "radio");
-	inputElement.setAttribute("name", "typeId");
-	inputElement.setAttribute("value", typeId);
-	inputElement.setAttribute("required", "true");
-
-	labelElement.setAttribute("for", "typeId");
-	labelElement.innerText = type.name;
-	
-	elementContainer.append(iconElement, labelElement, inputElement);
-
-	return elementContainer;
-};
-
-const CategoryFormType = (types: {[index: string]: InterfaceType}): HTMLDivElement => {
-	const element = document.createElement("div");
-	const elementOptions = Object.keys(types).map(typeKey => {
-		const element = composeCategoryFormTypeOption(typeKey, types[typeKey]);
-		return element
-	});
-
-	element.classList.add("category-form-parent");
-
-	element.append(...elementOptions);
-	return element;
-}
-
-export { CategoryFormType };
-
 const composeCategoryFormParentItem = (categoryKey: string, categoryName: string, categoryColor: string): HTMLDivElement => {
 	const elementContainer = document.createElement("div");
 	const inputElement = document.createElement("input");
