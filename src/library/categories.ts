70deb79ec33370c5de12f81a1f6f387fb74ffe64
Reinny <reynsth@gmail.com>
2021-03-16 12:49:42 -0400

    :art: Separating categories from default state

---
+++ src/library/categories.ts
@@ -0,0 +1,64 @@
+const categories = [
+	{
+		name: "Bills",
+		icon: "bills.svg"
+	},
+	{
+		name: "Entertainment",
+		icon: "entertainment.svg"
+	},
+	{
+		name: "Family",
+		icon: "family.svg"
+	},
+	{
+		name: "Food & Beverages",
+		icon: "food-and-beverages.svg"
+	},
+	{
+		name: "Gifts",
+		icon: "gift.svg"
+	},
+	{
+		name: "Groceries",
+		icon: "groceries.svg"
+	},
+	{
+		name: "Health",
+		icon: "health.svg"
+	},
+	{
+		name: "Household",
+		icon: "household.svg"
+	},
+	{
+		name: "Snacks",
+		icon: "snack.svg"
+	},
+	{
+		name: "Personal Development",
+		icon: "personal-development.svg"
+	},
+	{
+		name: "Shopping",
+		icon: "shopping.svg"
+	},
+	{
+		name: "Transportation",
+		icon: "transportation.svg"
+	},
+	{
+		name: "Trips",
+		icon: "trips.svg"
+	},
+	{
+		name: "Work",
+		icon: "work.svg"
+	},
+	{
+		name: "Other",
+		icon: "other.svg"
+	}
+];
+
+export { categories };
